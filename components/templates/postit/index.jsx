2a767ac3995bc499d4d0dfe5a3c3d5ddb37fd4ce
maliksaepul <malik@inspigo.id>
2021-12-10 06:09:06 +0700

    chore : update hero & postit

--- components/templates/postit/index.jsx
+++ components/templates/postit/index.jsx
@@ -33,7 +33,7 @@ const PostIt = props => {
                     cta={[
                         {
                             label: 'Learn More',
-                            src: 'https://padlet.com/broadcast5/PadletLFKamal2021',
+                            src: 'https://padlet.com/broadcast5/PadletLFYoel2021',
                             id: 'postit',
                         },
                     ]}>
