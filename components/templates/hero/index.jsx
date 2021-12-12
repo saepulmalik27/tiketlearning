1e7233ed10c10acdd83a43bd40adb4b7944bc504
maliksaepul <malik@inspigo.id>
2021-12-12 11:18:24 +0700

    update master event

--- components/templates/hero/index.jsx
+++ components/templates/hero/index.jsx
@@ -85,7 +85,7 @@ const Hero = props => {
                     </div>
                     <div className={cx(styles.item, styles.illu)}>
                         <Image
-                            src={'/images/agenda/hero_section_10dec-min.jpg'}
+                            src={'/images/agenda/hero_section_13dec-min.jpg'}
                             width="500"
                             height="500"
                         />
