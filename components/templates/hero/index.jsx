84ed1685ffe2d83bc0d6f862feb6f9d887e66c3f
maliksaepul <malik@inspigo.id>
2021-12-14 07:18:57 +0700

    chore : update hero asset

--- components/templates/hero/index.jsx
+++ components/templates/hero/index.jsx
@@ -85,7 +85,7 @@ const Hero = props => {
                     </div>
                     <div className={cx(styles.item, styles.illu)}>
                         <Image
-                            src={'/images/agenda/hero_section_14dec-min.png'}
+                            src={'/images/agenda/hero_section_14dec-min.jpg'}
                             width="500"
                             height="500"
                         />
